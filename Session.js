86d3c5cdb5554657b562aa0a88713144669690f0
Hidanzbotz <91834504+Hidanzbotz@users.noreply.github.com>
2021-11-27 15:02:39 +0800

    Delete Session.js

--- Session.js
+++
@@ -1,7 +0,0 @@
-{
-        "clientID": "/pew5IPiXNHmPIMF3yy5bA==",
-        "serverToken": "1@FLdmF6/S4WiJywuBg56GUA5IKcMv91S/wjZptuf8WqWiw9lmqYr6lgxsMxs1YoCphoKefU6nZtADjQ==",
-        "clientToken": "ca1HuAT2Uy1/lA74u9VTIZq3FzzypEMstOJesAXXZWA=",
-        "encKey": "ake7+WS3kgSqPq2/wwKpbXyP0eMh9Nc9kTk6c/YOFn0=",
-        "macKey": "u7ilcLFfs3dqc6bDEwGgqIuvSd7b8pawf3LqlnjZyl0="
-}
